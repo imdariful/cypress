bdec957568cc934f2264c8455a5e3b974875277c
Md Ariful Islam <hi@imdariful.com>
2024-03-10 11:16:36 +0600

    test report added

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,8 +1,8 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:5173/');
-    cy.get('#collapseTwo > .accordion-body > :nth-child(1) > .btn').click();
-    // cy.get('#collapseTwo > .accordion-body > :nth-child(11) > .btn').click();
+    // cy.get('#collapseTwo > .accordion-body > :nth-child(1) > .btn').click();
+    cy.get('#collapseTwo > .accordion-body > :nth-child(11) > .btn').click();
     cy.get('.card-title').contains('Rick Sanchez').click();
   });
 });
