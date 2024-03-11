bc17905e3f84d9697ed32c5acb4712a1a68e63d7
Md Ariful Islam <hi@imdariful.com>
2024-03-11 14:45:05 +0600

    test report added

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -7,6 +7,6 @@ describe('template spec', () => {
 
   it('passes', () => {
     cy.visit('http://localhost:5173/');
-    cy.get('#collapseTwo > .accordion-body > :nth-child(1) > .btn').click();
+    cy.get('#collapseTwo > .accordion-body > :nth-child(11) > .btn').click();
   });
 });
